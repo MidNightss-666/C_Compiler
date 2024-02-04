938484587d80089e397e7283c424ca17fa7b2d52
Midnights <3223586959@qq.com>
2024-02-04 16:52:19 +0800

    Changes

---
+++ Part1/cmake-build-debug/CMakeFiles/Part1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Part1.dir/main.cpp.obj"
+  "CMakeFiles/Part1.dir/main.cpp.obj.d"
+  "Part1.exe"
+  "Part1.exe.manifest"
+  "Part1.pdb"
+  "libPart1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Part1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
